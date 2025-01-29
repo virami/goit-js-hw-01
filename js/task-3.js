312a210f54c4f840ff3774e808f4b698d2579a81
virami <ihnasivv@gmail.com>
2025-01-29 20:13:35 +0200

    додано правки

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = Number.parseFloat(content);
+    const contentWidth = Number.parseFloat(content); 
     const paddingWidth = Number.parseFloat(padding) * 2;
     const borderWidth = Number.parseFloat(border) * 2;
     const totalWidth = contentWidth + paddingWidth + borderWidth;
@@ -7,3 +7,6 @@ function getElementWidth(content, padding, border) {
     return totalWidth;
 };
 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
